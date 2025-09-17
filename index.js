3e4059622a37e11e43a52ea71d0da91a2d21cd21
Ammar Elmihy <amaryasser2358@gmail.com>
2025-09-17 18:17:12 +0300

    connecting to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+// mongo connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("mongodb connected successfully!"));
 
 app.listen(port, () => {
   console.log("listening on port:", port);
